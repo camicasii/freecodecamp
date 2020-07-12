1357a4d4a510edb1621627d99228768845cb7f1b
camicasii <camicasii@hotmail.com>
2020-07-12 15:37:03 -0400

    code version 0.1

--- timestamp_microservice/index.js
+++ timestamp_microservice/index.js
@@ -4,6 +4,15 @@ const app =  express()
 
 app.set("port",process.env.PORT||2000)
 
+app.get('/api/timestamp/:date_string',(req,res)=>{
+    const {date_string} =req.params
+    console.log(date_string);    
+    if(date=="Invalid Date" || date_string==undefined)
+    return res.json({"error" : "Invalid Date" })    
+    let date = new Date(date_string)        
+    res.json({unix:date.getTime(),utc:date.toUTCString()})
+})
+
 
 
 app.listen(app.get('port'),()=>{
