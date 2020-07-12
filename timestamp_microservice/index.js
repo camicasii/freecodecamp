310299fb03e58f6af110dc50eceab06e8bfbeca6
camicasii <camicasii@hotmail.com>
2020-07-12 15:00:30 -0400

    start first project timestamp microservice

---
+++ timestamp_microservice/index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+
+const app =  express()
+
+app.set("port",process.env.PORT||2000)
+
+
+
+app.listen(app.get('port'),()=>{
+    console.log("server on port: ",app.get('port'));
+})
